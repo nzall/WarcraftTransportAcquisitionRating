499b92864c7ed099c709211cf44f843c76d8052d
Nate Kerkhofs <nzall@users.noreply.github.com>
2020-05-28 21:50:47 +0200

    Update warcraftTransportAcquisitionRating.lua
    Attempt at overriding the UpdateMountList script.

--- warcraftTransportAcquisitionRating.lua
+++ warcraftTransportAcquisitionRating.lua
@@ -1,13 +1,135 @@
 local wTAR = LibStub("AceAddon-3.0"):NewAddon("WarcraftTransportAcquisitionRating")
 
 function wTAR:OnEnable()
-    local mountsList = C_MountJournal.GetMountIDs()
-    for index,mountId in ipairs(mountsList) do
-        local currentMount = C_MountJournal.GetMountInfoByID()
-        if not currentMount.isCollected then
-            -- add a number from wTAR.data
-        end
-        
-    end
-    
+    MountJournal.ListScrollFrame:SetScript("OnUpdate", MountJournal_UpdateMountList);    
+end
+
+function MountJournal_UpdateMountList()
+	local scrollFrame = MountJournal.ListScrollFrame;
+	local offset = HybridScrollFrame_GetOffset(scrollFrame);
+	local buttons = scrollFrame.buttons;
+
+	local numMounts = C_MountJournal.GetNumMounts();
+	MountJournal.numOwned = 0;
+	local showMounts = true;
+	local playerLevel = UnitLevel("player");
+	if  ( numMounts < 1 ) then
+		-- display the no mounts message on the right hand side
+		MountJournal.MountDisplay.NoMounts:Show();
+		showMounts = false;
+	else
+		local mountIDs = C_MountJournal.GetMountIDs();
+		for i, mountID in ipairs(mountIDs) do
+			local _, _, _, _, _, _, _, _, _, hideOnChar, isCollected = C_MountJournal.GetMountInfoByID(mountID);
+			if (isCollected and hideOnChar ~= true) then
+				MountJournal.numOwned = MountJournal.numOwned + 1;
+			end
+		end
+		MountJournal.MountDisplay.NoMounts:Hide();
+	end
+
+	local numDisplayedMounts = C_MountJournal.GetNumDisplayedMounts();
+	for i=1, #buttons do
+		local button = buttons[i];
+		local displayIndex = i + offset;
+		if ( displayIndex <= numDisplayedMounts and showMounts ) then
+			local index = displayIndex;
+			local creatureName, spellID, icon, active, isUsable, sourceType, isFavorite, isFactionSpecific, faction, isFiltered, isCollected, mountID = C_MountJournal.GetDisplayedMountInfo(index);
+			local needsFanfare = C_MountJournal.NeedsFanfare(mountID);
+
+			button.name:SetText(creatureName);
+			button.icon:SetTexture(needsFanfare and COLLECTIONS_FANFARE_ICON or icon);
+			button.new:SetShown(needsFanfare);
+			button.newGlow:SetShown(needsFanfare);
+
+			button.index = index;
+			button.spellID = spellID;
+
+			button.active = active;
+			if (active) then
+				button.DragButton.ActiveTexture:Show();
+			else
+				button.DragButton.ActiveTexture:Hide();
+			end
+			button:Show();
+
+			if ( MountJournal.selectedSpellID == spellID ) then
+				button.selected = true;
+				button.selectedTexture:Show();
+			else
+				button.selected = false;
+				button.selectedTexture:Hide();
+			end
+			button:SetEnabled(true);
+			CollectionItemListButton_SetRedOverlayShown(button, false);
+			button.iconBorder:Hide();
+			button.background:SetVertexColor(1, 1, 1, 1);
+			if (isUsable or needsFanfare) then
+				button.DragButton:SetEnabled(true);
+				button.additionalText = nil;
+				button.icon:SetDesaturated(false);
+				button.icon:SetAlpha(1.0);
+				button.name:SetFontObject("GameFontNormal");
+			else
+				if (isCollected) then
+					CollectionItemListButton_SetRedOverlayShown(button, true);
+					button.DragButton:SetEnabled(true);
+					button.name:SetFontObject("GameFontNormal");
+					button.icon:SetAlpha(0.75);
+					button.additionalText = nil;
+					button.background:SetVertexColor(1, 0, 0, 1);
+				else
+					button.icon:SetDesaturated(true);
+					button.DragButton:SetEnabled(false);
+					button.icon:SetAlpha(0.25);
+					button.additionalText = "test";
+					button.name:SetFontObject("GameFontDisable");
+				end
+			end
+
+			if ( isFavorite ) then
+				button.favorite:Show();
+			else
+				button.favorite:Hide();
+			end
+
+			if ( isFactionSpecific ) then
+				button.factionIcon:SetAtlas(MOUNT_FACTION_TEXTURES[faction], true);
+				button.factionIcon:Show();
+			else
+				button.factionIcon:Hide();
+			end
+
+			if ( button.showingTooltip ) then
+				MountJournalMountButton_UpdateTooltip(button);
+			end
+		else
+			button.name:SetText("");
+			button.icon:SetTexture("Interface\\PetBattles\\MountJournalEmptyIcon");
+			button.index = nil;
+			button.spellID = 0;
+			button.selected = false;
+			CollectionItemListButton_SetRedOverlayShown(button, false);
+			button.DragButton.ActiveTexture:Hide();
+			button.selectedTexture:Hide();
+			button:SetEnabled(false);
+			button.DragButton:SetEnabled(false);
+			button.icon:SetDesaturated(true);
+			button.icon:SetAlpha(0.5);
+			button.favorite:Hide();
+			button.factionIcon:Hide();
+			button.background:SetVertexColor(1, 1, 1, 1);
+			button.iconBorder:Hide();
+		end
+	end
+
+	local totalHeight = numDisplayedMounts * MOUNT_BUTTON_HEIGHT;
+	HybridScrollFrame_Update(scrollFrame, totalHeight, scrollFrame:GetHeight());
+	MountJournal.MountCount.Count:SetText(MountJournal.numOwned);
+	if ( not showMounts ) then
+		MountJournal.selectedSpellID = nil;
+		MountJournal.selectedMountID = nil;
+		MountJournal_UpdateMountDisplay();
+		MountJournal.MountCount.Count:SetText(0);
+	end
 end
